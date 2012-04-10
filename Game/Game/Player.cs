2150b946a65becaa158e742ba8b1d1b6824fde9c
Evan Schimpf <evanschimpf@gmail.com>
2012-04-10 15:43:34 -0700

    Updated physics

--- Game/Game/Player.cs
+++ Game/Game/Player.cs
@@ -12,13 +12,37 @@ namespace Game
         public int health;
         public int acceleration;
         public int velocity;
+        public bool onTopOfBlock;
+
+        public bool airbourne;
+
+        public void jump()
+        {
+            velocity = -40;
+        }
 
         public override void Initialize(Texture2D initTexture, Vector2 initPosition)
         {
             base.Initialize(initTexture, initPosition);
             health = 5;
-            acceleration = 10;
+            acceleration = 3;
             velocity = 0;
+            onTopOfBlock = false;
+        }
+
+        public bool willCollide(Block obj2, int direction, int speed)
+        {
+            Rectangle obj1Rectangle = new Rectangle((int)position.X, (int)position.Y, width, height);
+            switch (direction)
+            {
+                case 1: obj1Rectangle.Y -= speed; break;    //up
+                case 2: obj1Rectangle.Y += speed; break;    //down
+                case 3: obj1Rectangle.X -= speed; break;    //left
+                case 4: obj1Rectangle.X += speed; break;    //right
+            }
+            Rectangle obj2Rectangle = new Rectangle((int)obj2.position.X, (int)obj2.position.Y, obj2.width, obj2.height);
+
+            return obj1Rectangle.Intersects(obj2Rectangle);
         }
     }
 }

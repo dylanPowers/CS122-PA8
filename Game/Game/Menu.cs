a6ba6d399f2811ffcbc07c037d64bad9c5ac332e
Dylan Powers <dylan.powers@email.wsu.edu>
2012-04-24 21:34:24 -0700

    Help (or directions) Completed

--- Game/Game/Menu.cs
+++ Game/Game/Menu.cs
@@ -15,38 +15,69 @@ namespace Game
         public bool is_active;
         private bool exit_flag;
         private bool restart_flag;
+        private bool is_directions_viewable;
 
+        //Restart Button
         private Texture2D restart_but;
         private Texture2D restart_but_clicked;
         private Texture2D restart_but_hover;
         private Texture2D restart_but_current;
+        private Vector2 restart_but_pos;
+
+        //Exit Button
         private Texture2D exit_but;
         private Texture2D exit_but_clicked;
         private Texture2D exit_but_hover;
         private Texture2D exit_but_current;
+        private Vector2 exit_but_pos;
+
+        //Background
         private Texture2D bg;
+        private Vector2 bg_pos;
+
+        //X Button
         private Texture2D x_but;
         private Texture2D x_but_clicked;
         private Texture2D x_but_current;
-        private Vector2 restart_but_pos;
-        private Vector2 exit_but_pos;
-        private Vector2 bg_pos;
         private Vector2 x_but_pos;
+
+        //Directions background
+        private Texture2D directions_bg;
+        private Vector2 directions_pos;
+
+        //Help Button
+        private Texture2D help_but_current;
+        private Texture2D help_but_rest;
+        private Texture2D help_but_hover;
+        private Texture2D help_but_clicked;
+        private Vector2 help_but_pos;        
+
         private MouseState prev_mouse;
 
         public void Initialize()
         {
+            is_directions_viewable = false;
             is_active = false;
             exit_flag = false;
             restart_flag = false;
+
             restart_but_pos.X = 400;
-            restart_but_pos.Y = 266;
+            restart_but_pos.Y = 232;
+
             exit_but_pos.X = 650;
-            exit_but_pos.Y = 266;
+            exit_but_pos.Y = 232;
+
             x_but_pos.X = 800;
             x_but_pos.Y = 125;
+
+            help_but_pos.X = 525;
+            help_but_pos.Y = 346;
+
             bg_pos.X = 313;
             bg_pos.Y = 88;
+
+            directions_pos.X = 0;
+            directions_pos.Y = 0;
         }
 
         public void LoadContent(ContentManager Content)
@@ -59,12 +90,17 @@ namespace Game
             exit_but_hover = Content.Load<Texture2D>("MenuContent/ExitButtonHover");
             x_but = Content.Load<Texture2D>("MenuContent/xButton");
             x_but_clicked = Content.Load<Texture2D>("MenuContent/xButtonClicked");
+            help_but_rest = Content.Load<Texture2D>("MenuContent/HelpButton");
+            help_but_clicked = Content.Load<Texture2D>("MenuContent/HelpButtonClicked");
+            help_but_hover = Content.Load<Texture2D>("MenuContent/HelpButtonHover");
 
             bg = Content.Load<Texture2D>("MenuContent/MenuBackground");
+            directions_bg = Content.Load<Texture2D>("MenuContent/DirectionsScreen");
 
             restart_but_current = restart_but;
             exit_but_current = exit_but;
             x_but_current = x_but;
+            help_but_current = help_but_rest;
            
 
         }
@@ -73,10 +109,15 @@ namespace Game
         {
             if (is_active)
             {
+                if (is_directions_viewable)
+                {
+                    sprites.Draw(directions_bg, directions_pos, Color.White);
+                }
                 sprites.Draw(bg, bg_pos, Color.White);
                 sprites.Draw(restart_but_current, restart_but_pos, Color.White);
                 sprites.Draw(exit_but_current, exit_but_pos, Color.White);
                 sprites.Draw(x_but_current, x_but_pos, Color.White);
+                sprites.Draw(help_but_current, help_but_pos, Color.White);
             }
         }
 
@@ -115,7 +156,7 @@ namespace Game
                     else if (mouse.LeftButton == ButtonState.Released && prev_mouse.LeftButton == ButtonState.Pressed)
                     {
                         restart_flag = true;
-                        is_active = false;
+                        exit();
                     }
                     else
                     {
@@ -136,7 +177,7 @@ namespace Game
                     else if (mouse.LeftButton == ButtonState.Released && prev_mouse.LeftButton == ButtonState.Pressed)
                     {
                         exit_flag = true;
-                        is_active = false;
+                        exit();
                     }
                     else
                     {
@@ -144,6 +185,7 @@ namespace Game
                     }
                 }
 
+                //Checks for the menu x button to exit the menu.
                 else if ((mouse.X <= x_but_pos.X + 43 && x_but_pos.X + 7 <= mouse.X)
                                                     &&
                          (mouse.Y <= x_but_pos.Y + 43 && x_but_pos.Y + 7 <= mouse.Y))
@@ -155,7 +197,7 @@ namespace Game
                     }
                     else if (mouse.LeftButton == ButtonState.Released && prev_mouse.LeftButton == ButtonState.Pressed)
                     {
-                        is_active = false;
+                        exit();
                     }
                     else
                     {
@@ -163,11 +205,34 @@ namespace Game
                     }
 
                 }
+
+                //Check for the help button
+                else if ((mouse.X <= help_but_pos.X + 140 && help_but_pos.X <= mouse.X)
+                                                    &&
+                         (mouse.Y <= help_but_pos.Y + 68 && help_but_pos.Y <= mouse.Y))
+                {
+                    if (mouse.LeftButton == ButtonState.Pressed)
+                    {
+                        help_but_current = help_but_clicked;
+
+                    }
+                    else if (mouse.LeftButton == ButtonState.Released && prev_mouse.LeftButton == ButtonState.Pressed)
+                    {                        
+                        is_directions_viewable = true;
+                    }
+                    else
+                    {
+                        help_but_current = help_but_hover;
+                    }
+
+                }
+
                 else
                 {
                     exit_but_current = exit_but;
                     restart_but_current = restart_but;
                     x_but_current = x_but;
+                    help_but_current = help_but_rest;
                 }
 
                 prev_mouse = mouse;
@@ -175,6 +240,12 @@ namespace Game
 
         }
 
+        private void exit()
+        {
+            is_active = false;
+            is_directions_viewable = false;
+
+        }
         public bool isPause()
         {
             if (is_active)
